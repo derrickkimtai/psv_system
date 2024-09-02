242dabbdd7cce806188a2a69113ae121f27551f3
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-09-02 11:08:21 +0300

    addded some forms for mager signup and login

---
+++ manager/forms.py
@@ -0,0 +1,17 @@
+from models import Route, Stage, Car
+from django import forms
+
+
+class ManagerSingupForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    fullname = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+
+class ManagerLoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from forms import ManagerLoginForm, ManagerSingupForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
+"""This is a fuction for the manager to sign up then login"""
+
+def manager_signup(request):
+    if request.method == 'POST':
+        form = ManagerSingupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            fullname = form.cleaned_data['fullname']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            password2 = form.cleaned_data['password2']
+            if password == password2:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                return HttpResponse('User created successfully')
+            else:
+                return HttpResponse('Password does not match')
+    else:
+        form = ManagerSingupForm()
+    return render(request, 'signup.html', {'form': form})
