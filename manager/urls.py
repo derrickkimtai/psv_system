2ab9eb2001150779042057b4d425e2cbc65f95c5
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-09-03 22:14:46 +0300

    added templates for the car route and the stage

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("manager_signup/", views.manager_signup, name="manager_signup"),
     path("manager_login/", views.manager_login, name="manager_login"),
     path("manager_dashboard/", views.manager_dashboard, name="manager_dashboard"),
-    path("manage_route/", views.manager_route, name="manage_route"),
-    path("manage_stage/", views.manager_stage, name="manage_stage"),
+    path("manage_route/", views.manage_route, name="manage_route"),
+    path("manage_stage/", views.manage_stage, name="manage_stage"),
     path("add_car/", views.add_car, name="add_car"),
 ]
