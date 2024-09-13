4e533ae9790f41d6813281d13e04017549a555ce
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-09-14 01:23:42 +0300

    added some features on the navbar

---
+++ cashier/static/js/index.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+        document.getElementById("nav").style.top = "0";
+    }else {
+        document.getElementById("nav").style.top = "-50px"
+    }
+}
