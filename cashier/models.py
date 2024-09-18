bad7c0013f7eb93a1e9dae9b9a90953313cb4633
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-09-18 14:16:50 +0300

    removed the  the boarding stage inthe model

--- cashier/models.py
+++ cashier/models.py
@@ -1,10 +1,4 @@
 from django.db import models
-from manager.models import Car, StagePrice
-
-# Create your models here.
-
-# cashier/models.py
-from django.db import models
 from manager.models import Car, Stage, Route, StagePrice
 from django.conf import settings
 
@@ -17,11 +11,10 @@ class Ticket(models.Model):
     number = models.CharField(max_length=100, default='0')
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='tickets')
     route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='tickets')
-    boarding_stage = models.ForeignKey(Stage, on_delete=models.CASCADE, related_name='boarding_tickets')
     alighting_stage = models.ForeignKey(Stage, on_delete=models.CASCADE, related_name='alighting_tickets')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     seat_number = models.IntegerField()
-    payment_method = models.CharField(max_length=100, choices=PAYMENT_METHOD_CHOICES, default='money')
+    payment_method = models.CharField(max_length=100, choices=PAYMENT_METHOD_CHOICES, default='Cash')
     cashier = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
